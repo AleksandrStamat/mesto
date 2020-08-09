ea2679bfd256fb00fa996463e6283aeee1f07ca2
Aleksandr <stamat.aleksandr@yandex.ru>
2020-08-10 01:58:17 +0300

    добавить popup-item

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 let popup = document.querySelector('.popup');
+let popupItem = document.querySelector('.popup-item')
 let popupOpen = document.querySelector('.profile__edit');
+let popupItemOpen = document.querySelector('.profile__add-button') // кнопка открытия попапа "Добавить"
 let popupClose = popup.querySelector('.popup__close');
+let popupItemClose = document.querySelector('.popup-item__close')
 let name = document.querySelector('.profile__name');
 let hobby = document.querySelector('.profile__hobby');
 let popupSave = popup.querySelector('.popup__form');
@@ -11,7 +14,6 @@ function openPopup(){
   nameInput.value = name.textContent;
   hobbyInput.value = hobby.textContent;
   popup.classList.add('popup_opened');
-
 }
 function closePopup(){
   popup.classList.remove('popup_opened');
@@ -22,8 +24,17 @@ function handlePopupSubmit(event) {
   hobby.textContent = hobbyInput.value;
   closePopup();
 }
+function openPopupItem(){                     // функция открытия попаппа "Добавить"
+  popupItem.classList.add('popup-item_opened');
+}
+function closeItemPopup(){
+  popupItem.classList.remove('popup-item_opened');
+}
 popupOpen.addEventListener('click', openPopup);
+popupItemOpen.addEventListener('click', openPopupItem); // слушатель попапа "Добавить"
 popupClose.addEventListener('click', closePopup);
+popupItemClose.addEventListener('click', closeItemPopup); // слушатель попапа "Добавить"
+
 popupSave.addEventListener('submit', handlePopupSubmit);
 
 
