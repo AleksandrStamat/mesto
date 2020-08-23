3464c169e2a3f877562f45b187be158f4461c052
Aleksandr <stamat.aleksandr@yandex.ru>
2020-08-23 14:36:48 +0300

    refactor: переработать index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,13 @@
-const initialCards = [
-  {
-    name: "Архыз",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 const popupProfile = document.querySelector(".popup-profile");
 const popupItem = document.querySelector(".popup-item");
 const popupImage = document.querySelector(".popup-image");
 
-let profileName = document.querySelector(".profile__name");
-let hobby = document.querySelector(".profile__hobby");
-let placeInput = document.querySelector("#place");
-let linkInput = document.querySelector("#link");
-let nameInput = document.querySelector("#name");
-let hobbyInput = document.querySelector("#hobby");
+const profileName = document.querySelector(".profile__name");
+const hobby = document.querySelector(".profile__hobby");
+const placeInput = document.querySelector("#place");
+const linkInput = document.querySelector("#link");
+const nameInput = document.querySelector("#name");
+const hobbyInput = document.querySelector("#hobby");
 
 const elementContainer = document.querySelector(".elements");
 const addForm = document.querySelector("#add-item");
@@ -58,12 +26,13 @@ const popupSave = document.querySelector(".popup__form");
 const popupItemSave = document.querySelector("#add-item"); // кнопка сохранения попапа "Добавить"
 
 function openPopup(popup) {
-  if (popup === popupProfile){
+  popup.classList.add("popup_opened");
+}
+function openFormHandler() {
+  openPopup(popupProfile)
   nameInput.value = profileName.textContent;
   hobbyInput.value = hobby.textContent;
-  }
-  popup.classList.add("popup_opened");
-} 
+}
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
@@ -79,44 +48,20 @@ function handlePopupSubmit(evt) {
 function handlePopupSubmitItem(evt) {
   // функция создания карточки добавления пользователем
   evt.preventDefault();
-  elementContainer.prepend(elementCreate(linkInput.value, placeInput.value));
+  elementContainer.prepend(createElement(linkInput.value, placeInput.value));
   addForm.reset();
   closePopup(popupItem);
 }
 
-const elementCreate = (link, name) => {
-  // создание карточки
-  const cardElement = document
-    .querySelector("#templateElement")
-    .content.cloneNode(true);
-  cardElement.querySelector(".element__foto").src = link;
-  cardElement.querySelector(".element__text").textContent = name;
-  cardElement.querySelector(".element__foto").alt = name;
-
-  const trash = cardElement.querySelector(".element__trash"); // кнопка удаления
-  trash.addEventListener("click", trashClick);
-
-  const likeButton = cardElement.querySelector(".element__like"); //кнопка лайка
-  likeButton.addEventListener("click", likeBtn);
-
-  const elementFoto = cardElement.querySelector(".element__foto");
-  elementFoto.addEventListener("click", () => popupImageOpen(link, name)); // слушатель нажатия  на картинку
-  return cardElement;
-};
-initialCards.forEach(function (item) {
-  // добавление карточек из массива
-  elementContainer.append(elementCreate(item.link, item.name));
-});
-
-function trashClick(evt) {
+function clickTrash(evt) {
   // функция удаления слушателей
   const removeElement = evt.target.closest(".element");
   const trash = removeElement.querySelector(".element__trash");
   const likeButton = removeElement.querySelector(".element__like");
   const elementFoto = removeElement.querySelector(".element__foto");
-  trash.removeEventListener("click", trashClick);
+  trash.removeEventListener("click", clickTrash);
   likeButton.removeEventListener("click", likeBtn);
-  elementFoto.removeEventListener("click", popupImageOpen);
+  elementFoto.removeEventListener("click", openPopupImage);
   removeElement.remove();
 }
 
@@ -125,15 +70,41 @@ function likeBtn(evt) {
   evt.target.classList.toggle("element__like_active");
 }
 
-function popupImageOpen(link, name) {
+function openPopupImage(link, name) {
   // функция открытия поапа картинки
   imageElement.src = link;
   imageText.textContent = name;
   openPopup(popupImage);
 }
 
+const createElement = (link, name) => {
+  // создание карточки
+  const cardElement = document
+    .querySelector("#templateElement")
+    .content.cloneNode(true);
+
+  const elementFoto = cardElement.querySelector(".element__foto");
+  elementFoto.addEventListener("click", () => openPopupImage(link, name)); // слушатель нажатия  на картинку
+
+  elementFoto.src = link;
+  cardElement.querySelector(".element__text").textContent = name;
+  elementFoto.alt = name;
+
+  const trash = cardElement.querySelector(".element__trash"); // кнопка удаления
+  trash.addEventListener("click", clickTrash);
+
+  const likeButton = cardElement.querySelector(".element__like"); //кнопка лайка
+  likeButton.addEventListener("click", likeBtn);
+
+  return cardElement;
+};
+initialCards.forEach(function (item) {
+  // добавление карточек из массива
+  elementContainer.append(createElement(item.link, item.name));
+});
+
 popupImageClose.addEventListener("click", () => closePopup(popupImage));
-popupOpen.addEventListener("click", () => openPopup(popupProfile));
+popupOpen.addEventListener("click", openFormHandler);
 popupItemOpen.addEventListener("click", () => openPopup(popupItem)); // слушатель попапа "кнопки Добавить"
 popupClose.addEventListener("click", () => closePopup(popupProfile));
 popupItemClose.addEventListener("click", () => closePopup(popupItem)); // слушатель попапа "кнопки Добавить"
